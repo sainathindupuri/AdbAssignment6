4fb16e07016e13d1d5d3ea2b29eba4ba41cb8ea6
sainath <sainathindupuri@gmail.com>
2022-08-09 14:23:54 -0500

    changes done

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import pyodbc
 from datetime import datetime
 from flask import Flask, Request,redirect, render_template, request, flash
 
+time = 15
 
 app = Flask(__name__, template_folder="templates")
 connection = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:adbsai.database.windows.net,1433;Database=adb;Uid=sainath;Pwd=Shiro@2018;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30')
 cursor = connection.cursor() 
-
-
+question = ""
+answer = ""
+score = 0
+totalScore = 0
 @app.route('/', methods=['POST', 'GET'])
 def Hello():
     return render_template('index.html')
@@ -16,10 +19,21 @@ def Hello():
 def Student():
     cursor = connection.cursor()
     query_str = "select * from MSGS a " 
-    print(query_str)
+    print(query_str)    
     cursor.execute(query_str+" ORDER BY a.DATE")
     data = cursor.fetchall()
-    return render_template('student.html', data = data)
+    return render_template('student.html', data = data, )
+
+@app.route('/SetTime', methods=['POST', 'GET'])
+def SetTime():
+   global time
+   time = int(request.form.get('time'))
+   return redirect("https://adbsainathquiz5.azurewebsites.net/Admin")
+
+    # query_str = "select * from MSGS a " 
+    # print(query_str)
+    # cursor.execute(query_str+" ORDER BY a.DATE")
+    # data = cursor.fetchall()
 
 @app.route('/StudentMessage', methods=['POST', 'GET'])
 def StudentMessage():
@@ -33,12 +47,47 @@ def StudentMessage():
     cursor.execute(query_str)
     cursor.commit()
 
-    # query_str = "select * from MSGS a " 
-    # print(query_str)
-    # cursor.execute(query_str+" ORDER BY a.DATE")
-    # data = cursor.fetchall()
+    query_str = "select top 1 * from MSGS a where a.ROLE = 'TEACHER'" 
+    print(query_str)
+    cursor.execute(query_str+" ORDER BY a.DATE DESC")
+    data = cursor.fetchone()
+    question = data[1]
+
+    query_str = "select top 1 * from MSGS a where a.ROLE = 'STUDENT'" 
+    print(query_str)
+    cursor.execute(query_str+" ORDER BY a.DATE DESC")
+    data = cursor.fetchone()
+    answer = data[1]
+
+    actualWords = []
+    score = 0
+    totalScore = 0
+    words = question.split(" ")
+    print("Words :",words)
+    for i in words:
+        print(i,i.isalpha())
+        if i.isalpha():
+            actualWords.append(i)
+            totalScore+=10
+    answerWords = answer.split(" ")
+    
+    for i in range(0, len(answerWords)):
+        print("Substrings are ",answerWords[i][1:])
+        if answerWords[i][1:] in actualWords:
+            score+=10
 
 
+    
+
+    now = datetime.now()
+    formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
+    scoreStr = "Score : "+str(score)+" Total Score : "+str(totalScore)
+    query_str = "INSERT INTO MSGS(ROLE, MESSAGE, DATE) VALUES('ADMIN','"+scoreStr+"','"+formatted_date+"')"
+    print(query_str)
+    cursor.execute(query_str)
+    cursor.commit()
+    
+    # return redirect("http://localhost:5000/Student")
     return redirect("https://adbsainathquiz5.azurewebsites.net/Student")
 
 
@@ -59,8 +108,8 @@ def TeacherMessage():
     # cursor.execute(query_str+" ORDER BY a.DATE")
     # data = cursor.fetchall()
 
-
-    return redirect("https://adbsainathquiz5.azurewebsites.net/Teacher")
+    # return redirect("http://localhost:5000/Teacher")
+    return redirect("https://adbsainathquiz5teacher.azurewebsites.net/Teacher")
 
 @app.route('/Teacher', methods=['POST', 'GET'])
 def Teacher():
@@ -74,11 +123,17 @@ def Teacher():
 @app.route('/Admin', methods=['POST', 'GET'])
 def Admin():
     cursor = connection.cursor()
+
+    
     query_str = "select * from MSGS a " 
     print(query_str)
     cursor.execute(query_str+" ORDER BY a.DATE")
     data = cursor.fetchall()
-    return render_template('Student.html', data = data)
+
+
+
+
+    return render_template('admin.html', data = data)
 
 
     
